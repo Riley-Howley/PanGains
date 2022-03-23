d173d3dd671427fa96738d834283d307780c0f98
Riley Howley <71965268+Riley-Howley@users.noreply.github.com>
2022-03-23 14:56:25 +1300

    Edited Register Screen
    Added Validation

--- lib/Screens/Auth Screens/signin_screen.dart
+++ lib/Screens/Auth Screens/signin_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:pangains/Http/requests.dart';
+import 'package:pangains/Screens/Dashboard/home_dashboard.dart';
 
 class SignInScreen extends StatelessWidget {
   const SignInScreen({Key? key}) : super(key: key);
-
+  static GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+    const snackBar = SnackBar(
+      content: Text(
+        'Error, Register Failed',
+        style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      ),
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -39,123 +49,145 @@ class SignInScreen extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          child: Column(
-            children: [
-              Center(
-                child: Container(
-                  margin: EdgeInsets.only(top: 64),
-                  child: Image.asset("assets/images/PanGains-black.jpg"),
+          child: Column(children: [
+            Center(
+              child: Container(
+                margin: EdgeInsets.only(top: 64),
+                child: Image.asset("assets/images/PanGains-black.jpg"),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              child: Text(
+                "Sign Up",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
-                margin: EdgeInsets.only(top: 32),
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 68),
+              width: MediaQuery.of(context).size.width,
+              height: 54,
+              child: SignInButton(
+                Buttons.FacebookNew,
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32),
+              width: MediaQuery.of(context).size.width,
+              height: 54,
+              child: SignInButton(
+                Buttons.Google,
+                onPressed: () {},
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 32, bottom: 70),
+              width: MediaQuery.of(context).size.width,
+              height: 54,
+              child: SignInButton(
+                Buttons.AppleDark,
+                onPressed: () {},
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 68),
-                width: MediaQuery.of(context).size.width,
-                height: 54,
-                child: SignInButton(
-                  Buttons.FacebookNew,
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 32),
-                width: MediaQuery.of(context).size.width,
-                height: 54,
-                child: SignInButton(
-                  Buttons.Google,
-                  onPressed: () {},
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 32, bottom: 70),
-                width: MediaQuery.of(context).size.width,
-                height: 54,
-                child: SignInButton(
-                  Buttons.AppleDark,
-                  onPressed: () {},
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width / 4,
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.black,
+                Container(
+                  margin: EdgeInsets.only(left: 30, right: 30),
+                  child: Text(
+                    "Or",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.only(left: 30, right: 30),
-                    child: Text(
-                      "Or",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width / 4,
+                  child: Divider(
+                    thickness: 2,
+                    color: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 69),
+                        child: Text(
+                          "Email",
+                          style: TextStyle(fontSize: 18),
+                        ),
                       ),
+                    ],
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your email",
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(top: 69),
+                        child: Text(
+                          "Password",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Enter your password",
                     ),
                   ),
                   Container(
-                    width: MediaQuery.of(context).size.width / 4,
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.black,
+                    margin: EdgeInsets.only(top: 64, bottom: 50),
+                    width: MediaQuery.of(context).size.width,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          if (_formKey.currentState!.validate()) {
+                            await Login(
+                                emailController.text, passwordController.text);
+                          }
+                          if (code != 200) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          } else if (code == 200) {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => HomeDashboardScreen(),
+                              ),
+                            );
+                          }
+                        }
+                      },
+                      child: Text("Sign In"),
                     ),
                   ),
                 ],
               ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "Email",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter your email",
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "Password",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Enter your password",
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 64, bottom: 50),
-                width: MediaQuery.of(context).size.width,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text("Sign In"),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ]),
         ),
       ),
     );
--- lib/Screens/Auth Screens/signup_screen.dart
+++ lib/Screens/Auth Screens/signup_screen.dart
@@ -7,24 +7,19 @@ import 'package:pangains/Screens/T&CS/t&cs_screen.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({Key? key}) : super(key: key);
-
+  static GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var fname = TextEditingController();
     var lname = TextEditingController();
     var email = TextEditingController();
     var password = TextEditingController();
-
-    Future errorDialog() => showDialog(
-          context: context,
-          builder: (context) => Dialog(
-            backgroundColor: Color(0xff222831),
-            child: Column(
-              children: [],
-            ),
-          ),
-        );
-
+    const snackBar = SnackBar(
+      content: Text(
+        'Error, Register Failed',
+        style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+      ),
+    );
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -136,134 +131,185 @@ class SignupScreen extends StatelessWidget {
                   ),
                 ],
               ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "First Name",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                controller: fname,
-                decoration: InputDecoration(
-                  hintText: "Enter your first name",
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "Last Name",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                controller: lname,
-                decoration: InputDecoration(
-                  hintText: "Enter your last name",
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "Email",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                controller: email,
-                decoration: InputDecoration(
-                  hintText: "Enter your email",
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 69),
-                    child: Text(
-                      "Password",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                ],
-              ),
-              TextField(
-                controller: password,
-                decoration: InputDecoration(
-                  hintText: "Enter your password",
-                ),
-              ),
-              Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(top: 64),
-                    child: Checkbox(
-                      value: true,
-                      onChanged: (check) {},
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 64),
-                    child: Text(
-                      "I agree to the",
-                      style: TextStyle(fontSize: 18),
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 64),
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => TermsAndConditionsScreens(),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 69),
+                          child: Text(
+                            "First Name",
+                            style: TextStyle(fontSize: 18),
                           ),
-                        );
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            value == null ||
+                            value.contains(
+                              RegExp(r'[0-9]'),
+                            )) {
+                          return 'Please enter your First Name, NO NUMBERS';
+                        }
+                        ;
                       },
-                      child: Text(
-                        "T&C's",
-                        style: TextStyle(fontSize: 18),
+                      controller: fname,
+                      decoration: InputDecoration(
+                        hintText: "Enter your first name",
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 65),
-                width: MediaQuery.of(context).size.width,
-                height: 48,
-                child: ElevatedButton(
-                  onPressed: () async {
-                    await Register(
-                        fname.text, lname.text, email.text, password.text);
-                    if (code != 200) {
-                      print("Error");
-                    } else {
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => UploadImageScreen(),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 69),
+                          child: Text(
+                            "Last Name",
+                            style: TextStyle(fontSize: 18),
+                          ),
                         ),
-                      );
-                    }
-                  },
-                  child: Text("Sign Up"),
-                ),
-              ),
-              Container(
-                margin: EdgeInsets.only(top: 32, bottom: 50),
-                child: Text(
-                  "Continue as guest",
-                  style: TextStyle(fontSize: 18, color: Color(0xff4f4f4f)),
+                      ],
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            value == null ||
+                            value.contains(
+                              RegExp(r'[0-9]'),
+                            )) {
+                          return 'Please enter your Last Name, NO NUMBERS';
+                        }
+                        ;
+                      },
+                      controller: lname,
+                      decoration: InputDecoration(
+                        hintText: "Enter your last name",
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 69),
+                          child: Text(
+                            "Email",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            value == null ||
+                            value.contains(
+                              RegExp(r'[0-9]'),
+                            ) ||
+                            !value.contains('@')) {
+                          return 'Please enter your Email';
+                        }
+                        ;
+                      },
+                      controller: email,
+                      decoration: InputDecoration(
+                        hintText: "Enter your email",
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 69),
+                          child: Text(
+                            "Password",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            value == null ||
+                            value.length <= 5) {
+                          return 'Please enter your Last Name, NO NUMBERS';
+                        }
+                        ;
+                      },
+                      controller: password,
+                      decoration: InputDecoration(
+                        hintText: "Enter your password",
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 64),
+                          child: Checkbox(
+                            value: true,
+                            onChanged: (check) {},
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 64),
+                          child: Text(
+                            "I agree to the",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 64),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      TermsAndConditionsScreens(),
+                                ),
+                              );
+                            },
+                            child: Text(
+                              "T&C's",
+                              style: TextStyle(fontSize: 18),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 65),
+                      width: MediaQuery.of(context).size.width,
+                      height: 48,
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await Register(fname.text, lname.text, email.text,
+                                password.text);
+                          }
+                          if (code != 200) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(snackBar);
+                          } else if (code == 200) {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => UploadImageScreen(),
+                              ),
+                            );
+                          }
+                        },
+                        child: Text("Sign Up"),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: 32, bottom: 50),
+                      child: Text(
+                        "Continue as guest",
+                        style:
+                            TextStyle(fontSize: 18, color: Color(0xff4f4f4f)),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
